460bc9b09eb4f3c7e783c9211fa57e7634268e9a
fernanda.barraza@berkeley.edu <fernandabarraza@Fernandas-MacBook-Air.local>
2024-10-19 08:48:48 -0700

    image prcessing

--- image.py
+++ image.py
@@ -1,17 +1,18 @@
+from pyexpat import model
 import requests
 import google.generativeai as genai
 import os
+from google.cloud import vision
 
 # Replace with your actual API key and the path to your service account key
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/Users/fernandabarraza/Desktop/mediscan-439109-6622ef5bbc56.json" 
-os.environ["YOUR_API_KEY"] = "YAIzaSyBaFUWQqp1qISn8pZOE2A06GUar5Bg8XAs"
+os.environ["YOUR_API_KEY"] = "AIzaSyBaFUWQqp1qISn8pZOE2A06GUar5Bg8XAs"
 
 genai.configure(api_key=os.environ["YOUR_API_KEY"])
 
 #Scanning image into text using Google Vision API
 def detect_text(path):
     """Detects text in the file."""
-    from google.cloud import vision
 
     client = vision.ImageAnnotatorClient()
 
@@ -27,20 +28,27 @@ def detect_text(path):
         return extracted_text
     else:
         return ""  # Return an empty string if no text is detected
+    
+if __name__ == "__main__":
+    medication_text = detect_text("/Users/fernandabarraza/Documents/images.jpeg")
 
-def get_drug_info(drug_name):
-    api_url = "https://api.gemini.example.com/drug"  # Replace with actual API endpoint
-    params = {
-        "name": drug_name
-    }
-    headers = {
-        "Authorization": "Bearer YOUR_API_KEY"  # Include your API key if required
-    }
-    response = requests.get(api_url, params=params, headers=headers)
+    response = model.generate_content("After reading this text: " + medication_text + " what medication is it and how many miligrams is it?")
 
-    if response.status_code == 200:
-        return response.json()  # Parse the JSON response
-    else:
-        print(f"Error: {response.status_code}, {response.text}")
-        return None
+    print(response.text)
+
+# def get_drug_info(drug_name):
+    # api_url = "https://api.gemini.example.com/drug"  # Replace with actual API endpoint
+    # params = {
+    #     "name": drug_name
+    # }
+    # headers = {
+    #     "Authorization": "Bearer YOUR_API_KEY"  # Include your API key if required
+    # }
+    # response = requests.get(api_url, params=params, headers=headers)
+
+    # if response.status_code == 200:
+    #     return response.json()  # Parse the JSON response
+    # else:
+    #     print(f"Error: {response.status_code}, {response.text}")
+    #     return None
 
