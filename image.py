b1abae8da47243b2654dff78298fb649fbf142e1
fernanda.barraza@berkeley.edu <fernandabarraza@Fernandas-MacBook-Air.local>
2024-10-19 22:07:28 -0700

    new flutter

--- image.py
+++ image.py
@@ -5,7 +5,7 @@ from google.cloud import vision
 model = genai.GenerativeModel(model_name="gemini-pro")
 
 # Replace with your actual API key and the path to your service account key
-os.environ["GOOGLE_APPLICATION_CRE] = "/Users/fernandabarraza/Desktop/mediscan-439109-6622ef5bbc56.json" 
+os.environ["GOOGLE_APPLICATION_CRE"] = "/Users/fernandabarraza/Desktop/mediscan-439109-6622ef5bbc56.json" 
 os.environ["YOUR_API_KEY"] = "AIzaSyBaFUWQqp1qISn8pZOE2A06GUar5Bg8XAs"
 
 genai.configure(api_key=os.environ["YOUR_API_KEY"])
@@ -30,9 +30,9 @@ def detect_text(path):
         return ""  # Return an empty string if no text is detected
     
 if __name__ == "__main__":
-    medication_text = detect_text("/Users/fernandabarraza/Documents/Tylenol-Extra-Strength-Caplets-with-500-mg-Acetaminophen-100-Ct_7fbb68c5-7b34-4c89-a54c-c9d17d4d2e20.7b8aa7dde99c41d6a005183b29865f6c.webp")
+    medication_text = detect_text("/Users/fernandabarraza/Documents/images (1).jpeg")
 
-    response = model.generate_content("After reading this text: " + medication_text + " what medication is it and how many miligrams is it?")
+    response = model.generate_content("After reading this text: " + medication_text + " what medication is it and how many miligrams is it? If it doesn't include miligrams, specify if syrup, gummy, etc. Else dont include.")
 
     print(response.text)
 
